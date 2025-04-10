61c0663edb379bf603162a34e8535a4dff3ff355
Shikhar Khandelwal <skykunnu@gmail.com>
2025-04-10 13:07:34 +0530

    Added a useEffect to take us to home section as soon as re rendering occurs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useEffect} from 'react'
 import Navbar from './components/Navbar'
 import Home from './sections/Home'
 import About from './sections/About'
@@ -9,6 +9,10 @@ import Experience from './sections/Experience'
 
 
 function App() {
+
+  useEffect(()=>{
+    window.location.hash='#home'
+  },[])
   return (
     
     <div className="bg-[#0a192f] text-white min-h-screen font-sans">
