7b572d4f99e326143ce41e0c8d9566b14b334ea4
Shikhar Khandelwal <skykunnu@gmail.com>
2025-04-21 00:54:14 +0530

    Add scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,69 @@
-import {useEffect} from 'react'
-import Navbar from './components/Navbar'
-import Home from './sections/Home'
-import About from './sections/About'
-import Skills from './sections/Skills'
-import Projects from './sections/Projects'
-import Contacts from './sections/Contacts'
-import Experience from './sections/Experience'
-
+import { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Home from './sections/Home';
+import About from './sections/About';
+import Skills from './sections/Skills';
+import Projects from './sections/Projects';
+import Contacts from './sections/Contacts';
+import Experience from './sections/Experience';
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      const scrollPos = window.scrollY + window.innerHeight / 2;
 
-  useEffect(()=>{
-    window.location.hash='#home'
-  },[])
+      for (let section of sections) {
+        const top = section.offsetTop;
+        const bottom = top + section.offsetHeight;
+
+        if (scrollPos >= top && scrollPos < bottom) {
+          const id = section.getAttribute('id');
+          if (window.location.hash !== `#${id}`) {
+            history.replaceState(null, '', `#${id}`);
+          }
+          break;
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // trigger once on mount
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    
     <div className="bg-[#0a192f] text-white min-h-screen font-sans">
       <Navbar />
-      <Home />
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Contacts />
+
+      <section id="home">
+        <Home />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="experience">
+        <Experience />
+      </section>
+
+      <section id="skills">
+        <Skills />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="contacts">
+        <Contacts />
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
